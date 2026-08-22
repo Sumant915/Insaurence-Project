43bb108f93420f95582b85b8aa3bce4255c5efb4
Sumant915 <singhsumantra920@gmail.com>
2026-08-22 21:15:00 +0530

    Configure production CORS

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://127.0.0.1:5500"
+        "https://insaurence-project-1.onrender.com"
     ],
     allow_credentials=True,
     allow_methods=["*"],
